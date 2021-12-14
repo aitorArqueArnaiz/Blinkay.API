8b905518fb5fe924354787b6582ced4cb500871f
aitoredualdnun <aarque@userzoom.com>
2021-12-14 18:29:14 +0100

    Finished select plus update use case

--- Blinkay.Domain/Services/MySqlService.cs
+++ Blinkay.Domain/Services/MySqlService.cs
@@ -1,4 +1,5 @@
 ﻿using Blinkay.Domain.Interfaces;
+using Blinkay.Domain.Shared;
 using Blinkay.Infrastructure.Entities;
 using NHibernate.Linq;
 using System;
@@ -44,9 +45,14 @@ namespace Blinkay.Domain.Services
                 try
                 {
                     var rnd = new Random();
-                    var rndId = rnd.Next(1, this._session.Users.Count());
+                    var rndId = rnd.Next(1, this._session.Users.Count() - 1);
                     var rndUser = this._session.Users.Where(u => u.Id == rndId).FirstOrDefault();
-                    rndUser.Info = "";
+                    while(rndUser == null)
+                    {
+                        rndId = rnd.Next(1, this._session.Users.Count() - 1);
+                        rndUser = this._session.Users.Where(u => u.Id == rndId).FirstOrDefault();
+                    }
+                    rndUser.Info = StringHelper.CreateRandomString(100);
 
                     _session.BeginTransaction();
                     await _session.SaveOrUpdate(rndUser);
---
+++ Blinkay.Domain/Shared/StringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Blinkay.Domain.Shared
+{
+    public static class StringHelper
+    {
+
+        /// <summary>
+        /// Method that creates/generated a new random string for user info DB column.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns>Random string.</returns>
+        public static string CreateRandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
