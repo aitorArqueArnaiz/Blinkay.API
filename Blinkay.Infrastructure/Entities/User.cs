fa30a6150c42fecee7653fc645f2bf59fc8d1e88
aitoredualdnun <aarque@userzoom.com>
2021-12-14 10:51:43 +0100

    Working on infrastructure with NHibernate tool

---
+++ Blinkay.Infrastructure/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Blinkay.Infrastructure.Entities
+{
+    public class User
+    {
+        /// <summary>
+        /// user table primary key.
+        /// </summary>
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User table information string/Json.
+        /// </summary>
+        [JsonProperty("Info")]
+        public string Info { get; set; }
+    }
+}
---
+++ Blinkay.Infrastructure/Maps/UserMap.cs
@@ -0,0 +1,27 @@
+﻿using Blinkay.Infrastructure.Entities;
+using NHibernate;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+public class UserMap : ClassMapping<User>
+{
+    public UserMap()
+    {
+        Id(x => x.Id, x =>
+        {
+            x.Generator(Generators.Increment);
+            x.Type(NHibernateUtil.Int64);
+            x.Column("iduser");
+            x.UnsavedValue(0);
+        });
+
+        Property(b => b.Info, x =>
+        {
+            x.Length(500);
+            x.Type(NHibernateUtil.StringClob);
+            x.NotNullable(true);
+        });
+
+        Table("users");
+    }
+}
